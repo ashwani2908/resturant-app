fd2e92ebeff7083bd5b16b9bfaa449d3f0c06a58
ashwani2908 <ishwani@outlook.com>
2023-08-15 12:12:56 +0530

    Created dummy meals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Header from "./component/Layout/Header";
+import AvailableMeals from "./component/Meals/AvailableMeals";
 
 function App() {
   return (
     <Fragment>
 
       <Header />
+      <AvailableMeals />
       
     </Fragment>
   );
